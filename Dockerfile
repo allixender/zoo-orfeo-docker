408030cf9f90a4d8d2456a5c54b0cfed0435ad14
Alex K <akmoch@aut.ac.nz>
2017-01-06 17:30:49 +1300

    travis docker build

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,19 @@
-FROM ubuntu:14.04
+FROM allixender/zoo-otb-base:latest
 
 # that's me!
 MAINTAINER Alex K, allixender@googlemail.com
 
-ENV CGI_DIR /usr/lib/cgi-bin
-ENV CGI_DATA_DIR /usr/lib/cgi-bin/data
-ENV CGI_TMP_DIR /usr/lib/cgi-bin/data/tmp
-ENV CGI_CACHE_DIR /usr/lib/cgi-bin/data/cache
-ENV WWW_DIR /var/www/html
+# ENVs should already come from base
+# ENV CGI_DIR /usr/lib/cgi-bin
+# ENV CGI_DATA_DIR /usr/lib/cgi-bin/data
+# ENV CGI_TMP_DIR /usr/lib/cgi-bin/data/tmp
+# ENV CGI_# EACHE_DIR /usr/lib/cgi-bin/data/cache
+# ENV WWW_DIR /var/www/html
 
 ADD build-script.sh /opt
 RUN chmod +x /opt/build-script.sh \
   && sync \
   && /opt/build-script.sh
-
-# RUN rm -rf $WWW_DIR/*
-RUN mkdir -p $CGI_DIR
-RUN mkdir -p $CGI_DATA_DIR
-RUN mkdir -p $CGI_TMP_DIR
-RUN mkdir -p $CGI_CACHE_DIR
 
 ADD cgi-bin $CGI_DIR/
 ADD data $CGI_DATA_DIR/
--- build-script.sh
+++ build-script.sh
@@ -3,7 +3,7 @@
 export RUNTIME_PACKAGES="wget libxml2 libcurlpp0 curl libcurl3 openssl apache2 libfcgi0ldbl libcairo2 libgeotiff2 libtiff5 \
 libgdal1h libgeos-3.4.2 libgeos-c1 libgd-dev libwxbase3.0-0 libgfortran3 libmozjs185-1.0 libproj0 \
 wx-common zip libwxgtk3.0-0 libjpeg62 libpng3 libxslt1.1 python2.7 apache2 uuid-dev libkml-java libkml0 \
-libmuparser2 libtinyxml2-0.0.0 libtinyxml2.6.2 libopenthreads14 libopenjpeg2 libossim1 ossim-core"
+libmuparser2 libtinyxml2-0.0.0 libtinyxml2.6.2 libopenthreads14 libopenjpeg2 libossim1 ossim-core cgal libcgal10"
 
 apt-get update -y \
       && apt-get install -y --no-install-recommends $RUNTIME_PACKAGES
@@ -13,7 +13,7 @@ build-essential libxslt1-dev software-properties-common libgdal-dev automake lib
  libgd-gd2-perl libgd2-xpm-dev ibwxbase3.0-dev  libwxgtk3.0-dev wx3.0-headers wx3.0-i18n libcurl4-gnutls-dev \
 libproj-dev libnetcdf-dev libfreetype6-dev libxslt1-dev libfcgi-dev libopenthreads-dev libcurlpp-dev \
 libtiff5-dev libgeotiff-dev swig2.0 cmake libkml-dev libmuparser-dev libtinyxml-dev libtinyxml2-dev \
-libopenjpeg-dev libboost-dev libboost1.54-dev libossim-dev"
+libopenjpeg-dev libboost-dev libboost1.54-dev libossim-dev libcgal10 libcgal-dev"
 
 apt-get install -y --no-install-recommends $BUILD_PACKAGES
 
@@ -31,85 +31,13 @@ export CGI_TMP_DIR=$CGI_DATA_DIR/tmp
 export CGI_CACHE_DIR=$CGI_DATA_DIR/cache
 export WWW_DIR=/var/www/html
 
-mkdir -p $BUILD_ROOT \
-  && mkdir -p $CGI_DIR \
-  && mkdir -p $CGI_DATA_DIR \
-  && mkdir -p $CGI_TMP_DIR \
-  && mkdir -p $CGI_CACHE_DIR \
-  && ln -s /usr/lib/x86_64-linux-gnu /usr/lib64
-
-wget -nv -O $BUILD_ROOT/mapserver-6.0.4.tar.gz http://download.osgeo.org/mapserver/mapserver-6.0.4.tar.gz \
-  && cd $BUILD_ROOT/ && tar -xzf mapserver-6.0.4.tar.gz \
-  && cd $BUILD_ROOT/mapserver-6.0.4 \
-  && ./configure --with-ogr=/usr/bin/gdal-config --with-gdal=/usr/bin/gdal-config \
-               --with-proj --with-curl --with-sos --with-wfsclient --with-wmsclient \
-               --with-wcs --with-wfs --with-kml=yes --with-geos \
-               --with-xml --with-xslt --with-threads --with-cairo \
-  && sed -i "s/-lgeos-3.4.2_c/-lgeos-3.4.2\ -lgeos_c/g" Makefile \
-  && sed -i "s/-lm -lstdc++/-lm -lstdc++ -ldl/g" Makefile \
-  && make && cp mapserv $CGI_DIR
-
-
-# here are the thirds
-ln -s /usr/lib/libfcgi.so.0.0.0 /usr/lib64/libfcgi.so \
-  && ln -s /usr/lib/libfcgi++.so.0.0.0 /usr/lib64/libfcgi++.so
-
-svn checkout http://svn.zoo-project.org/svn/trunk/thirds/ $BUILD_ROOT/thirds \
-  && cd $BUILD_ROOT/thirds/cgic206 && make
-
-# get ITK
-# http://www.itk.org/Wiki/ITK_Configuring_and_Building_for_Ubuntu_Linux
-# ITK_USE_REVIEW to OFF/ON regarding review API and license copyright etc
-# -DBUILD_EXAMPLES=ON/OFF
-wget -nv -O $BUILD_ROOT/InsightToolkit-4.7.2.tar.gz https://storage.googleapis.com/smart-server/InsightToolkit-4.7.2.tar.gz \
-  && cd $BUILD_ROOT/ && tar -xzf InsightToolkit-4.7.2.tar.gz \
-  && cd $BUILD_ROOT/InsightToolkit-4.7.2 \
-  && mkdir build \
-  && cd build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_PREFIX_PATH=/usr:/usr/local \
-    -DITK_USE_REVIEW=OFF \
-    -DBUILD_DOXYGEN=OFF \
-    -DBUILD_EXAMPLES=ON \
-    -DBUILD_SHARED_LIBS=ON \
-    -DBUILD_TESTING=OFF \
-    -DCMAKE_BACKWARDS_COMPATIBILITY=2.4 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DITK_USE_KWSTYLE=OFF ../ >../configure.out.txt \
-  && make -j2 && make install \
-  && cp -r bin/* /usr/bin/
-
-# /usr/include/ITK-4.7
-# /usr/lib/libITK...so...
-
-# export ITK_AUTOLOAD_PATH=/usr/bin
-# get otb
-# https://www.orfeo-toolbox.org/SoftwareGuide/SoftwareGuidech2.html
-# -DOTB_DATA_ROOT=/usr/lib/cgi-bin/data \
-# -DOTB_WRAP_PYTHON=ON \
-# -DOTB_WRAP_JAVA=OFF
-# -DOTB_BUILD_DEFAULT_MODULES=ON
-wget -nv -O $BUILD_ROOT/OTB-4.2.1.tgz https://storage.googleapis.com/smart-server/OTB-4.2.1.tgz \
-  && cd $BUILD_ROOT/ && tar -xzf OTB-4.2.1.tgz \
-  && cd $BUILD_ROOT/OTB-4.2.1 \
-  && mkdir build \
-  && cd build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr \
-    -DUSE_EXTERNAL_ITK=ON \
-    -DUSE_EXTERNAL_LIBKML=ON \
-    -DOTB_USE_LIBKML=OFF \
-    -DOTB_USE_OPENJPEG=ON \
-    -DOTB_USE_OPENCV=OFF \
-    -DOTB_USE_MUPARSER=ON \
-    -DOTB_USE_CURL=ON \
-    -DBUILD_EXAMPLES=OFF \
-    -DBUILD_TESTING=OFF ../ >../configure.out.txt \
-  && make -j2 && make install
-
-export ITK_AUTOLOAD_PATH=/usr/lib/otb/applications
-echo /usr/lib/otb >> /etc/ld.so.conf.d/otb.conf
-echo /usr/lib/otb/applications >> /etc/ld.so.conf.d/otb.conf
-/sbin/ldconfig
+# should build already there from base
+# mkdir -p $BUILD_ROOT \
+#   && mkdir -p $CGI_DIR \
+#   && mkdir -p $CGI_DATA_DIR \
+#   && mkdir -p $CGI_TMP_DIR \
+#   && mkdir -p $CGI_CACHE_DIR \
+#   && ln -s /usr/lib/x86_64-linux-gnu /usr/lib64
 
 svn checkout http://svn.zoo-project.org/svn/trunk/zoo-project/ $ZOO_BUILD_DIR \
   && cd $ZOO_BUILD_DIR/zoo-kernel && autoconf \
@@ -129,21 +57,6 @@ svn checkout http://svn.zoo-project.org/svn/trunk/zoo-project/ $ZOO_BUILD_DIR \
   --with-itk-version=4.7 \
   && make && make install
 
-# Add demo configs orfeo
-# otb2zcfg utility
-cd $BUILD_ROOT/thirds/otb2zcfg \
-  && mkdir build \
-  && cd build \
-  && cmake .. \
-  && make \
-  && mkdir zcfgs \
-  && cd zcfgs \
-  && ../otb2zcfg
-# otb2zcfg does not exit cleanly for some reason
-
-mkdir -p $CGI_DIR/OTB \
-  && cp -r *zcfg $CGI_DIR/OTB
-# cp *zcfg /location/to/your/cgi-bin/
 
 # however, auto additonal packages won't get removed
 # maybe auto remove is a bit too hard
@@ -154,11 +67,5 @@ mkdir -p $CGI_DIR/OTB \
 apt-get remove --purge -y $BUILD_PACKAGES \
   && rm -rf /var/lib/apt/lists/*
 
-rm -rf $BUILD_ROOT/mapserver-6.0.4
-rm $BUILD_ROOT/mapserver-6.0.4.tar.gz
-rm -rf $BUILD_ROOT/InsightToolkit-4.7.2
-rm $BUILD_ROOT/InsightToolkit-4.7.2.tar.gz
-rm -rf $BUILD_ROOT/OTB-4.2.1
-rm $BUILD_ROOT/OTB-4.2.1.tgz
-# rm -rf $ZOO_BUILD_DIR
+rm -rf $ZOO_BUILD_DIR
 # rm -rf $BUILD_ROOT/thirds
